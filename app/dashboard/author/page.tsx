979c4380a1c479fd8908d6730d71f6f3e58427d9
@chanrychhoy <chanry.c@aditi.asia>
2025-10-23 15:26:06 +0700

    test

--- app/dashboard/author/page.tsx
+++ app/dashboard/author/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Metadata } from 'next';
 
-import TableAuthor from '@/app/ui/authors/table';
+// import TableAuthor from '@/app/ui/authors/table';
 export const metadata: Metadata = {
   title: 'Authors',
 };
 export default function page() {
   return (
-  <TableAuthor />
+  // <TableAuthor />
   )
 }
